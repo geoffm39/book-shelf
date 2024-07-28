53e6bd8170cf64f1a4a407132dd9f7e9a5985044
Geoffrey Martin <geoffm39@gmail.com>
2024-07-29 00:00:53 +1000

    sql create db tables

---
+++ create-tables.sql
@@ -0,0 +1,18 @@
+CREATE TABLE author (
+	id SERIAL PRIMARY KEY,
+	api_id INTEGER UNIQUE NOT NULL,
+	name TEXT UNIQUE NOT NULL,
+	rating INTEGER CONSTRAINT valid_rating CHECK (rating > 0 AND rating <= 10)
+);
+
+CREATE TABLE book (
+	id SERIAL PRIMARY KEY,
+	api_id INTEGER UNIQUE NOT NULL,
+	title TEXT UNIQUE NOT NULL,
+	description TEXT,
+	date_read TIMESTAMP,
+	notes TEXT,
+	rating INTEGER CONSTRAINT valid_rating CHECK (rating > 0 AND rating <= 10),
+	author_id INTEGER REFERENCES author(id)
+);
+
