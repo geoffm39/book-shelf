e4a2f2ef4c1b944de874f71fe578a5c610875fb3
Geoffrey Martin <geoffm39@gmail.com>
2024-07-30 12:02:15 +1000

    complete header

--- index.js
+++ index.js
@@ -24,6 +24,10 @@ app.get("/", (req, res) => {
     res.render("index.ejs");
 });
 
+app.get("/add", (req, res) => {
+    res.render("book-form.ejs");
+});
+
 app.listen(process.env.SERVER_PORT, () => {
     console.log(`Server running on port ${process.env.SERVER_PORT}`);
 });
