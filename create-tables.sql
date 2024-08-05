24e59a44f86b418387230d6eafb6cbe7441b3572
Geoffrey Martin <geoffm39@gmail.com>
2024-08-05 23:53:36 +1000

    change rating form element to select

--- create-tables.sql
+++ create-tables.sql
@@ -4,7 +4,7 @@ CREATE TABLE author (
 	photo_id INTEGER,
 	name TEXT UNIQUE NOT NULL,
 	bio TEXT,
-	rating INTEGER CONSTRAINT valid_rating CHECK (rating > 0 AND rating <= 10)
+	rating INTEGER CONSTRAINT valid_rating CHECK (rating > 0 AND rating <= 5)
 );
 
 CREATE TABLE book (
@@ -15,7 +15,7 @@ CREATE TABLE book (
 	description TEXT,
 	date_read TIMESTAMP,
 	notes TEXT,
-	rating INTEGER CONSTRAINT valid_rating CHECK (rating > 0 AND rating <= 10),
+	rating INTEGER CONSTRAINT valid_rating CHECK (rating > 0 AND rating <= 5),
 	author_id INTEGER REFERENCES author(id)
 );
 
