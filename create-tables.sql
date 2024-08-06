08571e0cc6863cc7c358a1db2dc645490adc5efc
Geoffrey Martin <geoffm39@gmail.com>
2024-08-07 07:35:49 +1000

    remove unique restraint from some database fields

--- create-tables.sql
+++ create-tables.sql
@@ -2,7 +2,7 @@ CREATE TABLE author (
 	id SERIAL PRIMARY KEY,
 	api_id VARCHAR(50) UNIQUE NOT NULL,
 	photo_id INTEGER,
-	name TEXT UNIQUE NOT NULL,
+	name TEXT NOT NULL,
 	bio TEXT,
 	rating INTEGER CONSTRAINT valid_rating CHECK (rating > 0 AND rating <= 5)
 );
@@ -11,7 +11,7 @@ CREATE TABLE book (
 	id SERIAL PRIMARY KEY,
 	api_id VARCHAR(50) UNIQUE NOT NULL,
 	cover_id INTEGER,
-	title TEXT UNIQUE NOT NULL,
+	title TEXT NOT NULL,
 	description TEXT,
 	date_read TIMESTAMP,
 	notes TEXT,
